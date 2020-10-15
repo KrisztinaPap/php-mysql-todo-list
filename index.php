428c2f77674dd459e23b6d696d490cce85dc1433
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-15 14:06:01 -0600

    Adds HTML sections for different lists;

--- index.php
+++ index.php
@@ -8,13 +8,33 @@
 <body>
     <h1>MySQLi + PHP To-Do List</h1>
     <form action="#" method="POST" enctype="multipart/form-data">
+    <h2>Add New Task</h2>
         <p>
-            <label for="new_task">New Task</label>
+            <label for="new_task">Task</label>
             <input type="text" name="new_task" id="new_task">
+        </p>
+        <p>
+            <label for="due_date">Due date</label>
+            <input type="date" name="due_date" id="due_date">
+        </p>
+        <p>
+            <label for="category">Task category</label>
+            <select name="category" id="category">
+                <option value="">Choose one</option>
+            </select>
         </p>
         <p>
             <input type="submit" value="Add New Task">
         </p>
     </form>
+    <section>
+        <h2>Things to do</h2>
+    </section>
+    <section>
+        <h2>Overdue</h2>
+    </section>
+    <section>
+        <h2>Completed</h2>
+    </section>
 </body>
 </html>
