f1b504bc7539a48c5226945bcdaaf9d11f0e58dc
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-15 13:59:17 -0600

    Adds index.php and HTML skeleton;

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySQLi + PHP To-Do List</title>
+</head>
+<body>
+    <h1>MySQLi + PHP To-Do List</h1>
+    <form action="#" method="POST" enctype="multipart/form-data">
+        <p>
+            <label for="new_task">New Task</label>
+            <input type="text" name="new_task" id="new_task">
+        </p>
+        <p>
+            <input type="submit" value="Add New Task">
+        </p>
+    </form>
+</body>
+</html>
